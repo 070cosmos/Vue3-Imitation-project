311543f26c535c07ad604531168fa3ce7f41dd8d
070cosmos <Lyuhuaahu@163.com>
2023-05-25 15:51:12 +0800

    提交文件

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <div @click="toggleSidebar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <!-- update the cart - changed it to be dynamic -->
+      <span>Cart ({{ totalQuantity }})</span>
+    </div>
+  </header>
+  <router-view :inventory="inventory" :addToCart="addToCart" />
+
+  <!-- passing props (functions and variables) down to children -->
+  <!-- The children can call functions, update the state of the parent component -->
+  <Sidebar
+    v-if="showSidebar"
+    :toggle="toggleSidebar"
+    :cart="cart"
+    :inventory="inventory"
+    :remove="removeItem"
+  />
 </template>
 
+<!-- <style scoped></style> -->
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Sidebar from './components/Sidebar.vue'
+// @ is an alias to /src
+import Sidebar from "@/components/Sidebar.vue";
+import food from "@/food.json";
 
+// Options Object
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Sidebar,
+  },
+  data() {
+    return {
+      showSidebar: false,
+      inventory: food,
+      cart: {},
+    };
+  },
+  computed: {
+    totalQuantity() {
+      return Object.values(this.cart).reduce((acc, curr) => {
+        return acc + curr;
+      }, 0);
+    },
+  },
+  methods: {
+    addToCart(name, quantity) {
+      if (!this.cart[name]) this.cart[name] = 0;
+      // need to pass in the index when calling this addToCart function
+      // this.inventory[index].quantity -> quantity
+      this.cart[name] += quantity;
+      // clear the inputs/quantity by setting it to 0
+      // this.inventory[index].quantity = 0;
+      // console.log(this.cart);
+    },
+    toggleSidebar() {
+      this.showSidebar = !this.showSidebar;
+    },
+    removeItem(name) {
+      delete this.cart[name];
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+/* eslint-disable semi */
+<template>
+  <div class="home">
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+
+    <main class="wrapper">
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+        <!-- Loop over every item in the inventory and with slice only show the first three products -->
+        <!-- v-for="(product, i) in inventory.slice(0,3)" :key="i" -->
+        <ProductCard
+          v-for="(product, index) in inventory.slice(0, 3)"
+          :key="product.id"
+          class="card"
+          :index="index"
+          :product="product"
+          :addToCart="addToCart"
+        />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import ProductCard from "@/components/ProductCard.vue";
+// import ProductCard from "../components/ProductCard.vue";
+export default {
+  name: "Home",
+  props: ["inventory", "addToCart"],
+  components: {
+    ProductCard,
+  },
+};
+</script>
+
+ProductCard
